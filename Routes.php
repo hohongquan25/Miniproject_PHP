e9f54d6526beb64f3ae57fb0541a9c01a73fc100
hohongquan <hohongquan25@gmail.com>
2022-12-27 08:08:04 +0700

    login logout

--- Routes.php
+++ Routes.php
@@ -31,6 +31,15 @@ Route::set('delete', function() {
 Route::set('about-us', function() {
 		 AboutUs::CreateView('AboutUs');
 });
+<<<<<<< Updated upstream
 
+=======
+Route::set('login', function() {
+	loginController::CreateView('login');
+});
+Route::set('logout', function() {
+	logoutController::CreateView('logout');
+});
+>>>>>>> Stashed changes
 
 ?>
